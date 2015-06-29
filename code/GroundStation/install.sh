4202cf24214ad5c55286503dca565d15846ac4bd
Joseph Turner <joeman@leederville.net>
2015-06-29 21:25:33 +1000

    * Feedback in install.sh * Update web config to reflect different directory structure

--- code/GroundStation/install.sh
+++ code/GroundStation/install.sh
@@ -5,6 +5,7 @@
 #
 
 cp -pr web/* /data/web/
+chmod 777 /data/gs/gs.db
 
 [ -d /data/gs ] || mkdir /data/gs
 [ -d /data/gs/run ] || mkdir /data/gs/run
@@ -17,3 +18,9 @@ cp config.inc /data/gs/
 
 [ -f /data/gs/gs.db ] && mv /data/gs/gs.db /data/gs/gs.db.bck
 sqlite3 /data/gs/gs.db < tables.sql
+
+
+
+echo Finished
+
+echo May need to update /data/web/config.inc
