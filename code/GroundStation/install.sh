99db7a5664badead0e71101bc03079ea6aa50811
Joseph Turner <joeman@leederville.net>
2015-06-29 21:04:55 +1000

    * Adding steps to install database.

--- code/GroundStation/install.sh
+++ code/GroundStation/install.sh
@@ -6,10 +6,14 @@
 
 cp -pr web/* /data/web/
 
-mkdir /data/gs
-mkdir /data/gs/run
+[ -d /data/gs ] || mkdir /data/gs
+[ -d /data/gs/run ] || mkdir /data/gs/run
 chmod 777 /data/gs/run
 cp air_data.txt /data/gs/
 cp groundStation.pl /data/gs/
 cp gs.sh /data/gs/
 cp config.inc /data/gs/
+
+
+[ -f /data/gs/gs.db ] && mv /data/gs/gs.db /data/gs/gs.db.bck
+sqlite3 /data/gs/gs.db < tables.sql
