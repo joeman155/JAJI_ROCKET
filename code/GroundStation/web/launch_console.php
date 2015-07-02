d8a48ed6cba74bb93cd676fc0bf7592e8320429d
Joseph Turner <joeman@leederville.net>
2015-07-02 22:06:44 +1000

    * Starting to put launch console li stuff in.

--- code/GroundStation/web/launch_console.php
+++ code/GroundStation/web/launch_console.php
@@ -1,3 +1,30 @@
 <?
 
-echo "Works. Launch Control";
+# CONFIGURATION
+include "config.inc";
+
+# Get all the latest measurements
+try {
+     $dbh = new PDO("sqlite:" . $db_file);
+    }
+catch (PDOException $e)
+    {
+     echo $e->getMessage();
+    }
+
+?>
+
+<script>
+// AJAX stuff for buttons
+
+</script>
+<h3>Launch Console</h3>
+<div>
+<ul>
+  <li>Power-On</li>
+  <li>Arm</li>
+  <li>Continuinity test</li>
+  <li>Launch</li>
+</ul>
+</div>
+
