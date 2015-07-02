960fded281d7fc04a7227e4a57ffcd985bb87bcf
Joseph Turner <joeman@leederville.net>
2015-07-02 17:21:22 +1000

    * Modifying directory for DB.

--- code/GroundStation/install.sh
+++ code/GroundStation/install.sh
@@ -4,25 +4,35 @@
 # Purpose: To install Ground station
 #
 
+
+
+echo Creating GS directories
 [ -d /data/web ] || mkdir /data/web
 [ -d /data/web/out ] || mkdir /data/web/out
 [ -d /data/web/out/images ] || mkdir /data/web/out/images
-
-cp -pr web/* /data/web/
-
 [ -d /data/gs ] || mkdir /data/gs
+[ -d /data/gs/db ] || mkdir /data/gs/db
 [ -d /data/gs/run ] || mkdir /data/gs/run
+[ -d /data/gs/uploads ] || mkdir /data/gs/uploads
+
+echo Permissions on files
+cp -pr web/* /data/web/
+touch /data/gs/run/download_file_status
+chmod 777 /data/gs/db
 chmod 777 /data/gs/run
+chmod 777 /data/gs/uploads
+chmod 777 /data/gs/run/download_file_status
+
+echo Copying across files
 cp air_data.txt /data/gs/
 cp groundStation.pl /data/gs/
 cp gs.sh /data/gs/
 cp config.inc /data/gs/
-touch /data/gs/run/download_file_status
-chmod 777 /data/gs/run/download_file_status
 
 
-[ -f /data/gs/gs.db ] && mv /data/gs/gs.db /data/gs/gs.db.bck
-sqlite3 /data/gs/gs.db < tables.sql
+echo Initialising the database...
+[ -f /data/gs/db/gs.db ] && mv /data/gs/db/gs.db /data/gs/db/gs.db.bck
+sqlite3 /data/gs/db/gs.db < tables.sql
 chmod 777 /data/gs/gs.db
 
 
