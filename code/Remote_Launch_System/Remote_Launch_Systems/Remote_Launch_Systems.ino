903a5b0ba9105a7e04c4221f00c1281083c71df8
Joseph Turner <joeman@leederville.net>
2015-08-13 10:18:49 +1000

    * Logging of packets to log.txt done.

--- code/Remote_Launch_System/Remote_Launch_Systems/Remote_Launch_Systems.ino
+++ code/Remote_Launch_System/Remote_Launch_Systems/Remote_Launch_Systems.ino
@@ -15,11 +15,8 @@ int state;
 
 // SD Card and file declarations
 const int chipSelect = 4;
-const static char measurement_file[] PROGMEM = "measurements.txt";
 File myFile;
-Sd2Card card;
-SdVolume volume;
-SdFile root;
+int sdCardState = 0;  // 0 = uninitialised , -1 = error, 1 = initialised
 
 // Generic declarations
 char inData[20]; // Allocate some space for the string
@@ -64,70 +61,14 @@ void setup() {
   initLaunchSystem();
   resetLaunchSystem();
 
-
+  // Initialise microSD card.
   if (!SD.begin(chipSelect)) {
+    sdCardState = -1;
     sendPacket("E00");
-    Serial.println("E00");
-  } 
-
-
-/*
-  Serial.print("\nInitializing SD card...");
-  
-  // we'll use the initialization code from the utility libraries
-  // since we're just testing if the card is working!
-  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("* is a card inserted?");
-    Serial.println("* is your wiring correct?");
-    Serial.println("* did you change the chipSelect pin to match your shield or module?");
-    return;
   } else {
-    Serial.println("Wiring is correct and a card is present.");
+    sdCardState = 1;
   }
 
-  // print the type of card
-  Serial.print("\nCard type: ");
-  switch (card.type()) {
-    case SD_CARD_TYPE_SD1:
-      Serial.println("SD1");
-      break;
-    case SD_CARD_TYPE_SD2:
-      Serial.println("SD2");
-      break;
-    case SD_CARD_TYPE_SDHC:
-      Serial.println("SDHC");
-      break;
-    default:
-      Serial.println("Unknown");
-  }
-
-
-  // print the type and size of the first FAT-type volume
-  uint32_t volumesize;
-  Serial.print("\nVolume type is FAT");
-  Serial.println(volume.fatType(), DEC);
-  Serial.println();
-
-  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
-  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
-  volumesize *= 512;                            // SD card blocks are always 512 bytes
-  Serial.print("Volume size (bytes): ");
-  Serial.println(volumesize);
-  Serial.print("Volume size (Kbytes): ");
-  volumesize /= 1024;
-  Serial.println(volumesize);
-  Serial.print("Volume size (Mbytes): ");
-  volumesize /= 1024;
-  Serial.println(volumesize);
-
-
-  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
-  root.openRoot(volume);
-
-  // list all files in the card with date and size
-  root.ls(LS_R | LS_DATE | LS_SIZE);
-*/  
 }
 
 void loop() {
@@ -142,30 +83,6 @@ void loop() {
 
  // Air Pressure, Temperature
  // Prefix: D00
- myFile = SD.open("measure.txt", FILE_WRITE);
-
-
-  // if the file opened okay, write to it:
-  if (myFile) {
-    Serial.print("Writing to measurements.txt...");
-    myFile.println("testing 1, 2, 3.");
-    // close the file:
-    myFile.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
-   
-
-  // Check to see if the file exists:
-  if (SD.exists("measure.txt")) {
-    Serial.println("Measurement file exists.");
-  }
-  else {
-    Serial.println("Measurement doesn't exist.");
-  }
-  
   
   
 
@@ -317,6 +234,7 @@ void heartbeat()
 
 void sendPacket(String str) {
   Serial2.println(str);
+  logString(str);
 }
 
 
@@ -573,3 +491,19 @@ int disArmLaunchSystem() {
   return 1;
 }
 
+
+// Log string to log file
+void logString(String str) {
+  // ONLY proceed if no sdcard errors
+  if (sdCardState == 1) {
+     myFile = SD.open("log.txt", FILE_WRITE);
+
+     // if the file opened okay, write to it:
+     if (myFile) {
+       myFile.println(str);
+       myFile.close();
+     } else {
+      sdCardState = -1; // Error occured
+     }
+  } 
+}
