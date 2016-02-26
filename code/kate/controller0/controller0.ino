abfdd20acc9d5570b02788594b3ec3d99b3fb4dd
Joseph Turner <joeman@leederville.net>
2016-02-26 18:49:48 +1000

    * Changed debugging * Added in moment of inertia calc for 25 gram weight * Fixed timing.... so that there is adequate 'wait' after each end pulse is complete.

--- code/kate/controller0/controller0.ino
+++ code/kate/controller0/controller0.ino
@@ -137,7 +137,7 @@ void setup() {
   lower_velocity_threshold = 2;
   
   // KATE System set-up  - TESTING
-  torque_percent = 50;           // Safety margin...don't want to exceed max_torque...By reducing from 75 to 50..it seems to give a bit more of a safey factor...
+  torque_percent = 100;           // Safety margin...don't want to exceed max_torque...By reducing from 75 to 50..it seems to give a bit more of a safey factor...
                                  // allowing for additional time...should some other force on system be acting on the mass.
                                  // At present we are using 12 volts...we might be able to increase this if we want to use a higher voltage power source
   steps_per_rotation = 200;      // # of Steps per revolution
@@ -165,7 +165,8 @@ void setup() {
   //                         Moment of inertia of Arm rotation about axle                  Moment of inertia of spherical smoother                        Parallel axis thereom applied to Smoother                         
   moment_of_inertia =  (mass_of_arm * distance_to_smoother * distance_to_smoother/3)  + (2 * mass_of_smoother * smoother_radius * smoother_radius/5) +  (mass_of_smoother * distance_to_smoother * distance_to_smoother);
   moment_of_inertia = 0.005 * 0.002 * 0.002 /2;   // Moment of inertia of shaft...assuming it is steel (densisty = 8050), is a cylinder of length 45mm and radius 2mm)
-  moment_of_inertia = 200 * 0.005 * 0.002 * 0.002 /2;   // Just a calc I'm doing with arm...with no mass  THIS WORKS WELL...DO NO DELETE.
+  moment_of_inertia = 200 * 0.005 * 0.002 * 0.002 /2;   // Just a calc I'm doing with arm...with no mass  THIS WORKS WELL...DO NO DELETE....works best with torque_percent = 50
+  moment_of_inertia = 200 * 0.005 * 0.002 * 0.002 /2   + (0.025 *0.02 * 0.02);   // Just a calc I'm doing with arm...with AND  mass  THIS WORKS WELL...DO NO DELETE....works well wth torque_percent = 100
   
   // Deduce maximum rotational acceleration
   max_acceleration = ((torque_percent/(double) 100)* max_torque * (double) 0.001 * (double) 0.01 * (double) 9.81)/moment_of_inertia;
@@ -729,8 +730,9 @@ void move_stepper_motors(short s1_direction, short s2_direction, double angle, d
           cx_last = c0;
           
           // Serial.println("Moving Step: " + String(i) + String(" at time: ") + String(last_time_fired));
-          Serial.println(String(last_time_fired));
-          Serial.println(String(c0));
+          // Serial.println(String(last_time_fired));
+          Serial.print("STEP: " + String(i) + String("/") + String(steps/2) + String(", "));
+          Serial.println(String(0));
       } else {
           boolean finished_pulse = false;
           while (! finished_pulse) {
@@ -752,6 +754,7 @@ void move_stepper_motors(short s1_direction, short s2_direction, double angle, d
                   // Serial.println("Moving Step: " + String(i) + String(" at time: ") + String(last_time_fired));
                   // Serial.println(String(last_time_fired));
                   // Serial.println(String(next_step)+ String(", ") + String(actual_step));
+                  Serial.print("STEP: " + String(i) + String("/") + String(steps/2) + String(", "));
                   Serial.println(actual_step, DEC);
             
                 finished_pulse = true;
@@ -781,6 +784,7 @@ void move_stepper_motors(short s1_direction, short s2_direction, double angle, d
   while (i < steps_remaining) {
       // Serial.println("S:" + String(i));
     
+    /*
       // Do first pulse ASAP...no waiting
       if (first_triggered == true) {
           pulse_motor(MOTOR1_STEP);
@@ -796,6 +800,8 @@ void move_stepper_motors(short s1_direction, short s2_direction, double angle, d
           Serial.println(String(last_time_fired));
           Serial.println(String(cx_last));
       } else {
+      */  
+        
           boolean finished_pulse = false;
           while (! finished_pulse) {
               unsigned long current_time = micros();
@@ -816,13 +822,14 @@ void move_stepper_motors(short s1_direction, short s2_direction, double angle, d
                   // Serial.println("Moving Step: " + String(i) + String(" at time: ") + String(last_time_fired));
                   // Serial.println(String(last_time_fired));
                   // Serial.println(String(next_step)+ String(", ") + String(actual_step));
+                  Serial.print("STEP: " + String(i) + String("/") + String(steps/2) + String(", "));
                   Serial.println(actual_step, DEC);
             
                 finished_pulse = true;
               }
           }        
                 
-    }
+    // }
     
     
     i++;
@@ -840,6 +847,18 @@ void move_stepper_motors(short s1_direction, short s2_direction, double angle, d
     */
       
   }
+  
+  // Finished our pulses, but we need to wait until Step motor has completely stopped.
+  boolean finished_pulse = false;
+  while (! finished_pulse) {  
+    unsigned long current_time = micros();
+          if (current_time > next_time_fired) {
+            finished_pulse = true;
+            long actual_step = current_time - last_time_fired;
+            Serial.println("Final Wait: " + String(actual_step));
+          }
+  }
+  
   
   
   long end_time = micros();
